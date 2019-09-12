2c0b2d6910f1ebedf8e96f5ac5922ac7a2a5af04
autumn-ragland <raglanda998@gmail.com>
2019-09-12 14:20:46 -0500

    Graded 09-12-2019 14:17:42

--- exercise2.js
+++ exercise2.js
@@ -1,3 +1,5 @@
+// !! : button should start as disabled in your HTML
+
 //get node references from the DOM
 let box = document.getElementById("enter");
 let user_click =document.getElementById("down");
@@ -6,6 +8,7 @@ let second_click =document.getElementById("down");
 //set up event listener for button pressed to show text area
 user_click.addEventListener("click", btn_pressed);//reference to call back function
 
+// !! : toggle this behaviour 
 function btn_pressed(event) {
         user_click.removeAttribute("disabled");
         box.setAttribute("disabled", " ");
